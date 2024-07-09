02826a908bd1121f9d63e205cfef421a44b66b97
Guillermo Royo <guillermo.royo.rivera@gmail.com>
2024-07-08 22:32:51 -0400

    add changes to service and schema to prepare view

--- schema/product.py
+++ schema/product.py
@@ -2,10 +2,28 @@ from pydantic import BaseModel, Field
 from typing import Optional
 from datetime import datetime
 
+class Category(BaseModel):
+    id : Optional[int] = Field(None)
+    category_name : str = Field(..., max_length=100)
+    created_at: Optional[datetime] = Field(None)
+    updated_at: Optional[datetime] = Field(None)
+
+
 class Product(BaseModel):
     id : Optional[int] = Field(None)
     product_name : str = Field(..., max_length=100)
+    brand : Optional[str] = Field(None, max_length=100)
     product_description : Optional[str] = Field(None, max_length=255)
-    category: Optional[str] = Field(None, max_length=100)
+    category_id: Optional[int] = Field(None)
+    created_at: Optional[datetime] = Field(None)
+    updated_at: Optional[datetime] = Field(None)
+
+
+class Inventory(BaseModel):
+    id : Optional[int] = Field(None)
+    product_id: int = Field(...)
+    sector_id: Optional[int] = Field(None)
+    quantity : Optional[int] = Field(None)
+    expiration_date : Optional[datetime] = Field(None)
     created_at: Optional[datetime] = Field(None)
     updated_at: Optional[datetime] = Field(None)
--- services/product.py
+++ services/product.py
@@ -1,5 +1,5 @@
 from models.product import Product as ProductModel, Inventory as InventoryModel, Category as CategoryModel,  Sector as SectorModel
-from schema.product import Product
+from schema.product import Product, Category
 from sqlalchemy.orm import aliased
 
 class ProductService():
@@ -20,7 +20,7 @@ class ProductService():
         new_product = ProductModel(**product.model_dump())
         self.db.add(new_product)
         self.db.commit()
-        return True
+        return new_product
 
     def update_product(self, id: int, data: Product):
         product = self.db.query(ProductModel).filter(ProductModel.id == id).first()
@@ -56,3 +56,32 @@ class ProductService():
             product_alias.product_description.label('product_description'),
             product_alias.brand.label('product_brand')
         ).join(product_alias, InventoryModel.product).join(sector_alias, InventoryModel.sector).join(category_alias, product_alias.category).all()
+    
+    def get_or_create_category(self, category_name):
+
+        category = self.db.query(CategoryModel).filter(CategoryModel.category_name == category_name).first()
+        if not category:
+            category_schema = Category(category_name=category_name)
+            category = CategoryModel(**category_schema.model_dump())
+            self.db.add(category)
+            self.db.commit()
+        return category
+            
+    def create_inventory(self, inventory):
+        
+        new_inventory = InventoryModel(**inventory.model_dump())
+        self.db.add(new_inventory)
+        self.db.commit()
+        return new_inventory
+    
+
+    def delete_inventory(self, id: int):
+        
+        inventory = self.db.query(InventoryModel).filter(InventoryModel.id == id).first()
+        if inventory:
+            inventory_deleted = self.db.query(InventoryModel).filter(InventoryModel.id == id).delete()
+            product_id = inventory.product_id
+            self.delete_product(product_id)
+            self.db.commit()
+            return True
+        return False
