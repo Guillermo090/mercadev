4557a8a9c95b9aec233066c297862594945ad507
Guillermo Royo <guillermo.royo.rivera@gmail.com>
2024-08-15 18:50:56 -0400

    disable sqlalchemy logs

--- config/database.py
+++ config/database.py
@@ -8,7 +8,7 @@ base_dir = os.path.dirname(os.path.realpath(__file__))
 
 database_url = f"sqlite:///{os.path.join(base_dir, sqlite_datebase)}"
 
-engine = create_engine(database_url, echo=True)
+engine = create_engine(database_url, echo=False)
 
 Session = sessionmaker(bind=engine)
 
